5e30fbd732d17a08e5e6e2bf3a266337ce7d9a07
gadisa kasaye <gadisaka04@gmail.com>
2024-08-13 21:25:54 +0300

    correction

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -1,10 +1,14 @@
-str1 = "acb"
-str2 = "ahbgdc"
-str3 = ""
-for s in str1:
-    if s in str2:
-        str3 += s
-if str1 == str3:
-    print("true") 
-else:
-    print("false")
+if len(s) == 0 :
+    return True
+
+t_pointer = 0
+s_pointer = 0
+
+while t_pointer < len(t):
+    if t[t_pointer] == s[s_pointer] :
+        s_pointer += 1
+
+        if s_pointer == len(s):
+            return True
+    t_pointer += 1
+return False
