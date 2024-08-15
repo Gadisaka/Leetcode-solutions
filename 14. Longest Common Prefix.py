e0ce2fdb7f3dde677521a3e56e0999dc2709451f
gadisa kasaye <gadisaka04@gmail.com>
2024-08-15 09:43:14 +0300

    longest common prefix

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,13 @@
+strs = ["flower","flow","flight"]
+
+def fun():
+    res = ""
+    for i in range(len(strs[0])):
+        for s in strs:
+            if i == len(s) or s[i] != strs[0][i]:
+                return res
+        res += strs[0][i]
+    return res
+
+ans = fun()
+print(ans)
