2ff16b5bb90f1fd39b35f55de6f94a90a522cc89
gadisa kasaye <gadisaka04@gmail.com>
2024-08-16 09:46:40 +0300

    Summary Ranges

---
+++ 228. Summary Ranges.py
@@ -0,0 +1,19 @@
+nums = [0,1,2,4,5,7]
+
+def fun():
+    ans = []
+    i = 0
+    
+    while i < len(nums):
+        start = nums[i]
+        while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]:
+            i += 1
+        if start != nums[i]:
+            ans.append(str(start) + '->' + str(nums[i]) )
+        else:
+            ans.append(str(start))
+        i += 1
+    
+    return ans
+
+print(fun())
