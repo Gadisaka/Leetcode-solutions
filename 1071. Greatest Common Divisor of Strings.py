64b9a305825ab240788f0643d20c50eae70bd2f7
gadisa kasaye <gadisaka04@gmail.com>
2024-11-07 20:15:19 +0300

    1071

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,13 +1,10 @@
-# 1071. Greatest Common Divisor of Strings
-
-str1 = "Leet"
-str2 = "code"
-
+str1 = "ABCd"
+str2 = "ABCd"
 n1 = len(str1)
 n2 = len(str2)
 m = min(n1 , n2)
 
-strSet = []
+strSet = [ ]
 
 for i in range(m):
     if str1[i] == str2[i] and str1[i] not in strSet:
