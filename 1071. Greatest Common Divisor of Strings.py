7be6ca06ca99d1a67e47778e6f5941e5b336c634
gadisa kasaye <gadisaka04@gmail.com>
2024-11-15 08:06:13 +0300

    Refactor GCD of Strings solution to improve readability and efficiency

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,12 +1,13 @@
-str1 = "ABCd"
-str2 = "ABCd"
-n1 = len(str1)
-n2 = len(str2)
-m = min(n1 , n2)
+len1 , len2 = len(str1),len(str2)
+         
 
-strSet = [ ]
+def isDivisor(l):
+    if len1 % l or len2 % l:
+        return False
+    f1 , f2 = len1 // l , len2 // l
+    return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
 
-for i in range(m):
-    if str1[i] == str2[i] and str1[i] not in strSet:
-        strSet.append(str1[i])
-print("".join(strSet))
+for l in range(min(len1 , len2) , 0 , -1):
+    if isDivisor(l):
+        return str1[:l]
+return ""
